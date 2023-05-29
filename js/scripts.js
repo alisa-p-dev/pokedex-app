72d0db794b8ee95903adacd8c22418d395a60052
alisa-po <alisa.popova21@gmail.com>
2023-05-29 19:49:37 +0200

    added pockemonList to script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-alert('Hello world');
-let favoriteFood = 'Chocolate';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Squirtle",
+    height: 1.08,
+    types: ["water"],
+  },
+  {
+    name: "Charmander",
+    height: 2,
+    types: ["fire"],
+  },
+  {
+    name: "Miraidon",
+    height: 11.06,
+    types: ["electric", "dragon"],
+  },
+  {
+    name: "Enamorus",
+    height: 5.03,
+    types: ["fairy", "flying"],
+  }
+];
